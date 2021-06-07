e83d3f677a0988697c6e15acb296c9ed88db3440
Juan Chavez <jmchv2001@gmail.com>
2021-06-06 17:48:18 -0700

    views and partials handlebars updated; read-me updated

--- models/User.js
+++ models/User.js
@@ -13,7 +13,38 @@ User.init(
     {
         id: {
             type: Datatypes.INTEGER,
-            
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true            
+        },
+        username: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            } 
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
         }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
-)
+);
+
+
+module.exports = User;
